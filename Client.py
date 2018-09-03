e9ecdc6cac068bb1cf222a9a6918b76ef8462632
ciCic <koraypoyraz1@gmail.com>
2018-09-03 12:10:41 +0200

    Test

--- Client.py
+++ Client.py
@@ -6,3 +6,5 @@ from Audio import Audio
 print(AudioTranscribe.fromGoogleStorage(Audio('aphasiapatient.flac', 16000, 'en-GB', enums.RecognitionConfig.AudioEncoding.FLAC)))
 
 AudioTranscribe.fromAudioFile(Audio('aphasiapatientW.wav', 16000, 'en-GB', enums.RecognitionConfig.AudioEncoding.LINEAR16))
+
+# TESSSSST
